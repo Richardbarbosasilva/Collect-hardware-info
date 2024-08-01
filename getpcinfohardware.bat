704996a5e9551904aa828614a8550a3498b36e49
Richard Barbosa Silva <112967185+Richardbarbosasilva@users.noreply.github.com>
2024-08-01 08:45:37 -0400

    Update and bugfix getpcinfohardware.bat
    The powershell is set to run every 10 minutes to collect the desired data

--- getpcinfohardware.bat
+++ getpcinfohardware.bat
@@ -4,14 +4,15 @@ if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && 
 
 ::create some variables to tell the batch file where to find the powershell script.
 
-set "caminhopastaatual=%~dp0getpchardwaredata.ps1"
+set "caminhopastaatual=%~dp0getpcinfohardware.ps1"
 
 :: Verify if the file does exists
 
 if exist "%caminhopastaatual%" (
     echo powershell file found. Starting powershell in loop.
     :loop
-    powershell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -File "%~dp0getpchardwaredata.ps1" timeout /t 60 > nul
+    powershell.exe -WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -File "%~dp0getpcinfohardware.ps1" -verb runas
+    timeout /t 600 /nobreak
     goto :loop
 ) else (
     goto :Filenotfound
@@ -19,7 +20,8 @@ if exist "%caminhopastaatual%" (
 
 :Filenotfound
 
-echo getpcinfohardware.ps1 script not found, verify it's path and try once again.
+echo getpcinfohardware.ps1 script not found, verify its path and try once again.
+exit
 
 :: exit or pause
 
